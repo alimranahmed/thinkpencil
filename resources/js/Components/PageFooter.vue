a13c249e8031200488855751db5f403b541c13d1
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-05-31 09:07:06 +0600

    New layout implemented

---
+++ resources/js/Components/PageFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <inertia-link v-if="$page.props.auth.user" href="/dashboard" class="text-sm text-gray-700 underline">
+                Dashboard
+            </inertia-link>
+
+            <template v-else>
+                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
+                    Log in
+                </inertia-link>
+
+                <inertia-link v-if="canRegister" :href="route('register')"
+                              class="ml-4 text-sm text-gray-700 underline">
+                    Register
+                </inertia-link>
+            </template>
+        </div>
+        <div class="mt-4 py-4 border-t border-blue-200 text-center">
+            &copy; Al Imran Ahmed
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
